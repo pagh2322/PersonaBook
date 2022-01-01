09cc1c0d8ac9969d1fca90df863f1a26da75b0e8
pagh2322 <pagh2322@icloud.com>
2022-01-01 17:47:22 +0800

    Init Main Activity's UI

--- app/src/main/java/com/threesharp/personabook/MainActivity.java
+++ app/src/main/java/com/threesharp/personabook/MainActivity.java
@@ -1,14 +1,76 @@
 package com.threesharp.personabook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 
 import android.os.Bundle;
 
+import com.google.android.material.appbar.AppBarLayout;
+import com.threesharp.personabook.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    ArrayList<TypeWidget> typeList = new ArrayList<TypeWidget>();
+    TypeWidgetAdapter typeWidgetAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        typeWidgetAdapter = new TypeWidgetAdapter(typeList);
+        binding.rvType.setAdapter(typeWidgetAdapter);
+        binding.rvType.setLayoutManager(new GridLayoutManager(this,4));
+        init();
+        setContentView(binding.getRoot());
+    }
+
+
+    private void initTypeColor() {
+        TypeColor.ISTJ = getColor(R.color.ISTJ);
+        TypeColor.ISFJ = getColor(R.color.ISFJ);
+        TypeColor.INFJ = getColor(R.color.INFJ);
+        TypeColor.INTJ = getColor(R.color.INTJ);
+        TypeColor.ISTP = getColor(R.color.ISTP);
+        TypeColor.ISFP = getColor(R.color.ISFP);
+        TypeColor.INFP = getColor(R.color.INFP);
+        TypeColor.INTP = getColor(R.color.INTP);
+        TypeColor.ESTP = getColor(R.color.ESTP);
+        TypeColor.ESFP = getColor(R.color.ESFP);
+        TypeColor.ENFP = getColor(R.color.ENFP);
+        TypeColor.ENTP = getColor(R.color.ENTP);
+        TypeColor.ESTJ = getColor(R.color.ESTJ);
+        TypeColor.ESFJ = getColor(R.color.ESFJ);
+        TypeColor.ENFJ = getColor(R.color.ENFJ);
+        TypeColor.ENTJ = getColor(R.color.ENTJ);
+
+    }
+    public void init() {
+        initTypeColor();
+        addType("ISTJ-A/T", "4", TypeColor.ISTJ);
+        addType("ISFJ-A/T", "14", TypeColor.ISFJ);
+        addType("INFJ-A/T", "2", TypeColor.INFJ);
+        addType("INTJ-A/T", "0", TypeColor.INTJ);
+        addType("ISTP-A/T", "1", TypeColor.ISTP);
+        addType("ISFP-A/T", "12", TypeColor.ISFP);
+        addType("INFP-A/T", "21", TypeColor.INFP);
+        addType("INTP-A/T", "1", TypeColor.INTP);
+        addType("ESTP-A/T", "5", TypeColor.ESTP);
+        addType("ESFP-A/T", "0", TypeColor.ESFP);
+        addType("ENFP-A/T", "9", TypeColor.ENFP);
+        addType("ENTP-A/T", "14", TypeColor.ENTP);
+        addType("ESTJ-A/T", "2", TypeColor.ESTJ);
+        addType("ESFJ-A/T", "6", TypeColor.ESFJ);
+        addType("ENFJ-A/T", "15", TypeColor.ENFJ);
+        addType("ENTJ-A/T", "0", TypeColor.ENTJ);
+        typeWidgetAdapter.notifyDataSetChanged();
+    }
+    public void addType(String type, String num, int background) {
+        TypeWidget typeWidget = new TypeWidget();
+        typeWidget.setType(type);
+        typeWidget.setNumber(num);
+        typeWidget.setBackground(background);
+        typeList.add(typeWidget);
     }
 }
---
+++ app/src/main/java/com/threesharp/personabook/TypeColor.java
@@ -0,0 +1,22 @@
+package com.threesharp.personabook;
+
+import androidx.core.content.ContextCompat;
+
+public final class TypeColor {
+    public static int ISTJ;
+    public static int ISFJ;
+    public static int INFJ;
+    public static int INTJ;
+    public static int ISTP;
+    public static int ISFP;
+    public static int INFP;
+    public static int INTP;
+    public static int ESTP;
+    public static int ESFP;
+    public static int ENFP;
+    public static int ENTP;
+    public static int ESTJ;
+    public static int ESFJ;
+    public static int ENFJ;
+    public static int ENTJ;
+}
