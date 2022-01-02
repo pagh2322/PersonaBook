f155f9419fe24971aca2e382cf2cc06c46c0d643
pagh2322 <pagh2322@naver.com>
2022-01-02 16:51:09 +0800

    Add Custom Dialog

--- app/src/main/java/com/threesharp/personabook/AddPersonaActivity.java
+++ app/src/main/java/com/threesharp/personabook/AddPersonaActivity.java
@@ -4,16 +4,24 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.threesharp.personabook.databinding.ActivityAddPersonaBinding;
 
 public class AddPersonaActivity extends AppCompatActivity {
     private ActivityAddPersonaBinding binding;
+    private Dialog cancelDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,10 @@ public class AddPersonaActivity extends AppCompatActivity {
     }
 
     private void init() {
+        initToolbar();
+        initDialog();
+    }
+    private void initToolbar() {
         setSupportActionBar(binding.toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
@@ -49,19 +61,29 @@ public class AddPersonaActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-    private void loadDialog() {
-        AlertDialog.Builder dlg = new AlertDialog.Builder(AddPersonaActivity.this);
-        dlg.setMessage("취소할까요?");
-        dlg.setPositiveButton("취소",new DialogInterface.OnClickListener(){
-            public void onClick(DialogInterface dialog, int which) {
+    private void initDialog() {
+        cancelDialog = new Dialog(AddPersonaActivity.this);
+        cancelDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        cancelDialog.setContentView(R.layout.custom_dialog);
+        cancelDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        TextView tvNo = cancelDialog.findViewById(R.id.tv_no);
+        tvNo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cancelDialog.dismiss();
+            }
+        });
+        TextView tvYes = cancelDialog.findViewById(R.id.tv_yes);
+        tvYes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 finish();
             }
         });
-        dlg.show();
     }
 
     @Override
     public void onBackPressed() {
-        loadDialog();
+        cancelDialog.show();
     }
 }
